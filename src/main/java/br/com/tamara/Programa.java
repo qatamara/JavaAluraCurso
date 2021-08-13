772ab6bbd007d914de857ea61e0b081bb8df3c2f
Tamara Aquino <tamara.silva@softdesign.com.br>
2021-08-13 15:43:32 -0300

    first commit

---
+++ src/main/java/br/com/tamara/Programa.java
@@ -0,0 +1,9 @@
+package br.com.tamara;
+
+public class Programa {
+    public static void main(String[] args ){
+        System.out.println("Harry style");
+        System.out.println("Olá, mundo!!");
+        System.out.println("Sou QAtamara");
+    }
+}
